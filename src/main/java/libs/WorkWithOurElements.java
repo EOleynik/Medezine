b1ef4b6c80a665fc46fca89c664598fecb2189a0
User <Malbox@i.ua>
2019-07-07 22:44:13 +0300

    selectTextInDropDown was added

--- src/main/java/libs/WorkWithOurElements.java
+++ src/main/java/libs/WorkWithOurElements.java
@@ -5,6 +5,10 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.yandex.qatools.htmlelements.element.Link;
+import ru.yandex.qatools.htmlelements.element.TextBlock;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
 public class WorkWithOurElements {
     WebDriver webDriver;
@@ -14,11 +18,11 @@ public class WorkWithOurElements {
         this.webDriver = webDriver;
     }
 
-    public void enterTextToInput (WebElement element, String text){
+    public void enterTextToInput (TypifiedElement element, String text){
         try{
             element.clear();
             element.sendKeys(text);
-            loger.info(text + " was inputted into input");
+            loger.info(text + " was inputted into" + " " + element.getName());
         }catch(Exception e){
             loger.error("Can not work with element");
             Assert.fail("Can not work with element");
@@ -26,9 +30,19 @@ public class WorkWithOurElements {
     }
 
     public void clickOnElement(WebElement webElement) {
-        try {
+        try{
             webElement.click();
             loger.info("Element was clicked");
+        }catch (Exception e) {
+            loger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+
+    public void clickOnElement(TypifiedElement webElement) {
+        try {
+            webElement.click();
+            loger.info("Element" + " " + webElement.getName() + " " + "was clicked");
         } catch (Exception e) {
             loger.error("Can not work with element");
             Assert.fail("Can not work with element");
@@ -51,5 +65,10 @@ public class WorkWithOurElements {
         }catch (Exception e){
             return false;
         }
+    }
+
+    public void selectTextInDropDown(TypifiedElement dropDown, String text) {
+        clickOnElement(dropDown);
+        clickOnElement(".//option[@value='https://medizine.eu/brands/deflu/']");
     }
 }
