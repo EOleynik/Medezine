d0cdd0d41e0a5a28c0e3935e3223b1d29cf61917
User <Malbox@i.ua>
2019-07-07 17:28:31 +0300

    LoginTest was added

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -1,56 +1,30 @@
 package loginTest;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.HomePage;
+import parentTest.ParentTest;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
+public class LoginTest extends ParentTest {
 
-public class LoginTest {
+    @Test
+    public void validLogin(){
+    homePage.openPage();
+    homePage.checkCurrentUrl();
+    homePage.clickOnLinkMoyAccaunt();
 
-    WebDriver webDriver;
-    @Before
-    public void setUp(){
-        File file = new File("./src/drivers/chromedriver1.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+    myAccountPage.checkCurrentUrl();
+    myAccountPage.enterTextInToInputUserName("avtotest");
+    myAccountPage.enterTextInToInputPass("avtotest19");
+    myAccountPage.clickButtonVoiti();
 
-        webDriver = new ChromeDriver();
-
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    checkExpectedResult("Authorization failed, user info is not present",
+            true,myAccountPage.IsUserInfoPresent());
     }
 
     @Test
-    public void validLogIn(){
+    public void unvalidLogin(){
+        myAccountPage.loginWithCred("avtotest","avtotest");
 
-
-        webDriver.get("https://medizine.eu/");
-
-        webDriver.findElement(By.xpath(".//li[@id='menu-item-3048']")).click();
-        webDriver.findElement(By.xpath(".//input[@id='username']")).clear();
-        webDriver.findElement(By.xpath(".//input[@id='username']")).sendKeys("avtotest");
-
-        webDriver.findElement(By.xpath(".//input[@id='password']")).clear();
-        webDriver.findElement(By.xpath(".//input[@id='password']")).sendKeys("avtotest19");
-
-        webDriver.findElement(By.xpath(".//button[@name='login']")).click();
-
-        HomePage homePage = new HomePage(webDriver);
-
-        Assert.assertTrue("User-info is not present",homePage.IsUserInfoPresent());
-
-
+        checkExpectedResult("User info should not be present",
+                false,myAccountPage.IsUserInfoPresent());
     }
-
-    @After
-    public void tearDown(){
-        webDriver.quit();
-    }
-
 }
