ae9887ca2fa61c1a917f4d9737f0c1dfabb1e393
User <Malbox@i.ua>
2019-07-07 17:26:42 +0300

    clickOnLinkMoyAccaunt was added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,18 +3,25 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.logging.Logger;
 
 public class HomePage extends ParentPage{
 
+    @FindBy(xpath = ".//a[@href='/my-account/orders/' and @class='menu-link']")
+    private WebElement MoyAccaunt;
+    @FindBy(xpath = ".//div[@id='site-logo-inner']")
+    private WebElement logotype;
+
     public HomePage(WebDriver webDriver) {
-        super(webDriver);
+        super(webDriver,"");
     }
 
     public void openPage() {
         try {
-            webDriver.get("https://medizine.eu/");
+            webDriver.get("https://medizine.eu");
             logger.info("HomePage was opened");
         } catch (Exception e) {
             logger.error("Can not open HomePage");
@@ -22,12 +29,12 @@ public class HomePage extends ParentPage{
         }
     }
 
-        public boolean IsUserInfoPresent () {
-            try {
-                return webDriver.findElement(By.xpath(".//div[@class='user-info']")).isDisplayed();
-            } catch (Exception e) {
-                return false;
-            }
-        }
+    public void clickOnLinkMoyAccaunt() {
+    workWithOurElements.clickOnElement(MoyAccaunt);
     }
+
+    public boolean isLogotypePresen() {
+        return workWithOurElements.isElementDisplayed(logotype);
+    }
+}
 
