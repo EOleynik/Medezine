4b6209a4bcef21c025ba762270e0126810d050a7
User <Malbox@i.ua>
2019-07-07 17:35:52 +0300

    login with params

---
+++ src/test/java/loginTest/UnvalidLoginWithParams.java
@@ -0,0 +1,38 @@
+package loginTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import parentTest.ParentTest;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+@RunWith(Parameterized.class)
+public class UnvalidLoginWithParams extends ParentTest {
+    String userName, pass;
+
+    public UnvalidLoginWithParams(String userName, String pass) {
+        this.userName = userName;
+        this.pass = pass;
+    }
+
+    @Parameterized.Parameters(name = "Parametrs are {0} and {2}")
+    public static Collection testData(){
+        return Arrays.asList(new Object [][]{
+                {"avtotest","avtotest"},
+                {"avtotest","pass"},
+                {"login","pass"}
+        });
+    }
+
+    @Test
+    public void unvalidLogin (){
+        myAccountPage.loginWithCred(userName,pass);
+
+        checkExpectedResult("User info should not be present",
+                false,myAccountPage.IsUserInfoPresent());
+    }
+
+}
