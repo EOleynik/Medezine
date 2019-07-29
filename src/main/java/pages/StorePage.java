a3d3ada6ee273543d09ea345da3f708c88444fed
User <Malbox@i.ua>
2019-07-29 23:34:35 +0300

    checkStatusCheckBox, scrollByVisibleElement, clickOnButtonPrimenit were added

--- src/main/java/pages/StorePage.java
+++ src/main/java/pages/StorePage.java
@@ -1,9 +1,12 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.*;
+
+import java.util.ArrayList;
 
 import static jdk.nashorn.internal.objects.NativeString.substring;
 
@@ -28,13 +31,15 @@ public class StorePage extends ParentPage {
     private TextBlock inputDo;
     @FindBy(xpath = ".//button[@class='button']")
     private Button buttonPrimenit;
+    @FindBy(xpath = ".//button[text()='Применить фильтр']")
+    private WebElement primenitFiltr;
 
     public StorePage(WebDriver webDriver) {
         super(webDriver, "store/");
     }
 
     public void selectNameBrandFromDropDown(String nameBrand) {
-    workWithOurElements.selectTextInDropDown(brands,nameBrand);
+        workWithOurElements.selectTextInDropDown(brands, nameBrand);
     }
 
     public void clickOnButtonVkorziny() {
@@ -46,7 +51,7 @@ public class StorePage extends ParentPage {
     }
 
     public void scrollDown(Integer pix) {
-        workWithOurElements.ScrollByPixel(pix);
+        workWithOurElements.scrollByPixel(pix);
     }
 
     public void clickOnElementToolbarFiltr() {
@@ -55,16 +60,16 @@ public class StorePage extends ParentPage {
 
     public void scrollByVisibleElementFıltrPoTsene() {
         JavascriptExecutor js = (JavascriptExecutor) webDriver;
-        fıltrPoTsene = (WebElement) webDriver.findElement (By.xpath(".//button[text()='Применить фильтр']"));
-        js.executeScript ("arguments [0] .scrollIntoView ();", fıltrPoTsene);
+        fıltrPoTsene = (WebElement) webDriver.findElement(By.xpath(".//button[text()='Применить фильтр']"));
+        js.executeScript("arguments [0] .scrollIntoView ();", fıltrPoTsene);
     }
 
     public void enterTextInToInputOt(String value1) {
-        workWithOurElements.enterTextToInput(inputOt,value1);
+        workWithOurElements.enterTextToInput(inputOt, value1);
     }
 
     public void enterTextInToInputDo(String value2) {
-        workWithOurElements.enterTextToInput(inputDo,value2);
+        workWithOurElements.enterTextToInput(inputDo, value2);
     }
 
     public void clickOnButtonPrimenit() {
@@ -73,11 +78,11 @@ public class StorePage extends ParentPage {
 
     public String checkBrand(String nameBrand) {
         return (webDriver.findElement
-                (By.xpath(".//a[contains(text(),'" +nameBrand+ "')]")
+                (By.xpath(".//a[contains(text(),'" + nameBrand + "')]")
                 ).getText().split(" ")[0]);
     }
 
-    public boolean checkPrice( String value1, String value2) {
+    public boolean checkPrice(String value1, String value2) {
         if (Double.parseDouble(webDriver.findElement
                 (By.xpath(".//ul[@class='woo-entry-inner clr']/li[@class='inner']/span")
                 ).getText().split(" ")[0]) > Double.parseDouble(value1)) {
@@ -86,13 +91,43 @@ public class StorePage extends ParentPage {
                     ).getText().split(" ")[0]) < Double.parseDouble(value2)) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
 
     public double getPrice() {
-       return (Double.parseDouble(webDriver.findElement
+        return (Double.parseDouble(webDriver.findElement
                 (By.xpath(".//ul[@class='woo-entry-inner clr']/li[@class='inner']/span")
-                ).getText().substring(0,6)));
+                ).getText().substring(0, 6)));
     }
-}
 
+    public void checkStatusCheckBox(String checkBox) {
+        if (webDriver.findElement(By.xpath
+                (".//div[@class='pwb-filter-products']//ul//li//*[contains(@value,'" + checkBox + "')]")).isSelected()) {
+        } else webDriver.findElement(By.xpath
+                (".//div[@class='pwb-filter-products']//ul//li//*[contains(@value,'" + checkBox + "')]")).click();
+    }
+
+
+    public void scrollByVisibleElementPrimenitFiltr() {
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+        primenitFiltr = (WebElement) webDriver.findElement(By.xpath(".//button[text()='Применить фильтр']"));
+        js.executeScript("arguments [0] .scrollIntoView ();", primenitFiltr);
+    }
+
+    public void clickOnButtonPrimenitFiltr() {
+        workWithOurElements.clickOnElement(primenitFiltr);
+    }
+
+    public boolean checkProductsBrand(String nameBrand) {
+        ArrayList<WebElement> arrayList = new ArrayList<WebElement>();
+        arrayList.addAll(webDriver.findElements
+                (By.xpath(".//a[contains(text(),'" + nameBrand + "')]")));
+         for (int i = 0; i < arrayList.size(); i++)
+         {Assert.assertEquals(nameBrand,arrayList.get(i).getText().split(" ")[0]);
+        }
+        return true;
+    }
+
+
+}
